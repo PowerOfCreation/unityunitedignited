484d3c81d59e0604afaed67cd5443eed20994fa2
PowerOfCreation <questerdesura@gmail.com>
2019-11-09 18:50:58 +0100

    Player sprite and walk animation

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -7,25 +7,33 @@ public class Movement : MonoBehaviour
 {
     public float speed = 5;
     public Transform playerSprite;
+    public Animator animator;
     private Rigidbody2D rigidbody2d;
+
+    int walkId;
 
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        walkId = Animator.StringToHash("walking");
     }
 
     void FixedUpdate()
     {
-        //TODO - Movement auf animation erweitern
-        //Drehen von spieler
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         Vector2 moveDirection = gameObject.GetComponent<Rigidbody2D>().velocity;
+
         if (moveDirection != Vector2.zero)
         {
             float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
             playerSprite.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+            animator.SetBool(walkId, true);
+        }
+        else
+        {
+            animator.SetBool(walkId, false);
         }
         rigidbody2d.AddForce(movement * speed * Time.deltaTime);
     }
