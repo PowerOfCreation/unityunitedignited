ad1e0aab7b32c89e8cb248caa0ac206c8744dfc9
PowerOfCreation <questerdesura@gmail.com>
2019-11-09 21:25:26 +0100

    Lighting and torch

---
+++ Assets/Scripts/Fackel/PulsingLightRadius.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class PulsingLightRadius : MonoBehaviour
+{
+    private Light _light;
+    private float startRadius;
+
+    private void Start()
+    {
+        _light = GetComponent<Light>();
+        startRadius = _light.range;
+    }
+
+    private void FixedUpdate()
+    {
+        float sinTime = Mathf.Sin(Time.fixedTime * 3f);
+        _light.range = this.startRadius + sinTime * 0.2f;
+    }
+}
