4690a51968978d2d1bce57dbdcb8f1c8feb1751f
Reinice <57558931+Reinice@users.noreply.github.com>
2019-11-09 14:29:11 +0100

    Added rotation to the movement

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -19,8 +19,13 @@ public class Movement : MonoBehaviour
         //Drehen von spieler
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        Vector2 moveDirection = gameObject.GetComponent<Rigidbody2D>().velocity;
+        if (moveDirection != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        }
         rigidbody2d.AddForce(movement * speed * Time.deltaTime);
     }
 }
