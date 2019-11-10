b8d23b5be07cdcd7c9586d1fc4840a5b013d4d86
PowerOfCreation <questerdesura@gmail.com>
2019-11-10 10:41:30 +0100

    Alcohol now heals

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -4,32 +4,38 @@ using UnityEngine;
 
 public class PlayerHealth : Health, IDamagable
 {
-    public int playerHealth = 10;
+    private int maxPlayerHealth;
+
+    public void Start()
+    {
+        maxPlayerHealth = health;
+    }
 
     public override void GetDamage(int damage)
     {
-        playerHealth -= damage;
-        HealthSlider.self.slider.value = playerHealth; 
+        health -= damage;
+        UpdateHealthSlider();
 
-        if (playerHealth <= 0)
+        if (health <= 0)
         {
             Die();
         }
+    }
+
+    public virtual void Heal(int amount)
+    {
+        health += amount;
+        health = (health > maxPlayerHealth)?maxPlayerHealth:health;
+        UpdateHealthSlider();
     }
 
     public override void Die()
     {
         GameStateManager.Instance.SetGameState(GameStateManager.GameState.Over);
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void UpdateHealthSlider()
     {
-     
+        HealthSlider.self.slider.value = health; 
     }
 }
