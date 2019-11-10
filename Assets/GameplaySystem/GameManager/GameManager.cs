b819b9838e3b2ac1a4338e6be248647433715e14
PowerOfCreation <questerdesura@gmail.com>
2019-11-10 15:39:38 +0100

    GameManager now takes care of new level

--- Assets/GameplaySystem/GameManager/GameManager.cs
+++ Assets/GameplaySystem/GameManager/GameManager.cs
@@ -1,13 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
     private Transform playerTransform;
 
     public float minDistanceOfObjectsToPlayer = 10f;
-    
+
+    private int currentLevel = 0;
+    private int levelCountBeforeBossLevel = 3;
+
+    private int mainMenuId = 0;
+    private int normalLevelId = 1;
+    private int bossLevelId = 2;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(normalLevelId);
+    }
+
+    public void NextLevel()
+    {
+        currentLevel++;
+
+        if(currentLevel == levelCountBeforeBossLevel)
+        {
+            SceneManager.LoadScene(bossLevelId);
+        }
+        else
+        {
+            SceneManager.LoadScene(normalLevelId);
+        }
+    }
+
     public Transform GetLocalPlayer()
     {
         return playerTransform;
