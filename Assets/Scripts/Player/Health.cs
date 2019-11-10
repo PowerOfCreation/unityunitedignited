050bb732313a7a535c463934deb5d7285d81d018
Reinice <57558931+Reinice@users.noreply.github.com>
2019-11-10 10:55:16 +0100

    Enemies Go Red When Hit

--- Assets/Scripts/Player/Health.cs
+++ Assets/Scripts/Player/Health.cs
@@ -7,17 +7,53 @@ public class Health : MonoBehaviour, IDamagable
 {
     public int health = 10;
 
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rigidbody2D;
+    private bool tookDamage = false;
+    private int damageTime = 20;
+
+    private GameObject player;
+
     public virtual void GetDamage(int damage)
     {
+        //Go Red When Hit
+        spriteRenderer.color = new Color(1, 0, 0);
+        tookDamage = true;
+        //Take Damage
         health -= damage;
         if (health <= 0) 
         {
             Die();
         }
+        //Kockback
+        rigidbody2D.AddForce(transform.forward * 5000);
+
+        Debug.Log("Schaden!!!");
     }
 
     public virtual void Die()
     {
         Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        rigidbody2D = GetComponentInChildren<Rigidbody2D>();
+        player = GameObject.FindWithTag("Player");
+    }
+
+    private void Update()
+    {
+        if (tookDamage)
+        {
+            damageTime--;
+            if (damageTime <= 0)
+            {
+                spriteRenderer.color = Color.white;
+                tookDamage = false;
+                damageTime = 20;
+            }
+        }
     }
 }
