09ef9fa175daa183a0b0c6889d6aae895ec5cc29
Reinice <57558931+Reinice@users.noreply.github.com>
2019-11-10 18:10:24 +0100

    Main Menu

---
+++ Assets/LoadMainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadMainMenu : MonoBehaviour
+{
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
