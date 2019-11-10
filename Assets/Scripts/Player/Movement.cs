9a8e6317ffce0a2e32db540cf60dc23bf1e9f7a6
Reinice <57558931+Reinice@users.noreply.github.com>
2019-11-10 12:34:41 +0100

    Added Dash

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -9,6 +9,10 @@ public class Movement : MonoBehaviour
     public Transform playerSprite;
     private Rigidbody2D rigidbody2d;
 
+    public float dashSpeed;
+    private float dashTime;
+    public float startDashTime;
+
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
@@ -22,11 +26,39 @@ public class Movement : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         Vector2 moveDirection = gameObject.GetComponent<Rigidbody2D>().velocity;
+
         if (moveDirection != Vector2.zero)
         {
             float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
             playerSprite.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
         }
+
         rigidbody2d.AddForce(movement * speed * Time.deltaTime);
+
+        if (dashTime < 0)
+        {
+            dashTime = startDashTime;
+            rigidbody2d.velocity = Vector2.zero;
+        }
+        else
+        {
+            dashTime -= Time.deltaTime;
+            if (Input.GetButtonDown("Shift") && moveDirection.x > 0)
+            {
+                rigidbody2d.velocity = Vector2.right * dashSpeed;
+            }
+            else if (Input.GetButtonDown("Shift") && moveDirection.x < 0)
+            {
+                rigidbody2d.velocity = Vector2.left * dashSpeed;
+            }
+            else if (Input.GetButtonDown("Shift") && moveDirection.y > 0)
+            {
+                rigidbody2d.velocity = Vector2.up * dashSpeed;
+            }
+            else if (Input.GetButtonDown("Shift") && moveDirection.y < 0)
+            {
+                rigidbody2d.velocity = Vector2.up * dashSpeed;
+            }
+        }
     }
 }
